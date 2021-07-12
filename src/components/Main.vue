33c0214d5295181a78fbdef59f7575344c68ed86
Urek98 <gianluca.argo1998@libero.it>
2021-07-12 17:34:02 +0200

    completata sezione Main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,31 @@
 <template>
-    <div>Main</div>
+  <div class="main-section">
+      <div class="container">
+          <div class="text-main">
+            --Contents Goes here--
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Main'
-}
+  name: "Main",
+};
 </script>
+
+<style lang="scss" scoped>
+    .main-section{
+        background-color: rgb(28,28,28);
+        height: 120px;
+        .container{
+            height: 100%;
+            .text-main{
+                color: white;
+                font-size: 20px;
+                font-family: oswald;
+                padding-top: 40px;
+            }
+        }
+    }
+</style>
